fbe219205d130ebadf7feec248b90ccf814d8dbc
Kunyang Xie <k47xie@uwaterloo.ca>
2021-09-28 07:42:54 -0400

    Finish add mod rm

--- a1/a1ece650.py
+++ a1/a1ece650.py
@@ -3,58 +3,75 @@ import sys
 
 # YOUR CODE GOES HERE
 
-class Map:
-    def __init__(self, streetName, node):
-        self.streeName = streetName
-        self.node = node
-
-def add(streetName, node, map):
-    print("This is add")
-
-def mod(streetName, node, map):
-    print("This is mod")
-
-def rm(streetName, node, map):
-    print("This is rm")
-
-def gg():
-    print("This is gg")
-
-def getStreetName(line):
-    length = len(line)
-    index = []
-    for i in range(length):
-        if(line[i] == '"'): # If we found "
-            index.append(i) # return the index of two " in line
-    streetName = line[index[0]+1: index[1]] # Contents in the middle of two "
-    return streetName
-
-def getNode(line):
-    length = len(line)
-    node = []
-    for i in range(length):
-        if(line[i] == ','): # If we found ,
-            node.append(line[i - 1])  
-            node.append(line[i + 1])    # add content before and after ,
-    return node
-
-def check_func(line):
-    cmd = line[:3]
-    streetName = getStreetName(line)
-    node = getNode(line)
-    map = Map()
+class Street:
+    def __init__(self, command, map):
+        self.command = command
+        self.map = map
     
-    if(cmd == 'add'):
-        add(streetName, node, map)
-    elif(cmd == 'mod'):
-        mod(streetName, node, map)
-    elif(cmd == 'rm '):
-        rm(streetName, node, map)
-    elif(cmd == 'gg '):
-        gg()
+    def getStreetName(self):
+        length = len(self.command)
+        index = []
+        for i in range(length):
+            if(self.command[i] == '"'): # If we found "
+                index.append(i) # return the index of two " in line
+        streetName = self.command[index[0]+1: index[1]] # Contents in the middle of two "
+        return streetName
+
+    def getNode(self):
+        length = len(self.command)
+        node = []
+        for i in range(length):
+            if(self.command[i] == ','): # If we found ,
+                node.append(self.command[i - 1])  
+                node.append(self.command[i + 1])    # add content before and after ,
+        return node
+    
+    def capsulate(self, name, node):    # Capsulate data into dictionary
+        street = {
+            "Name": name,
+            "Node": node
+        }
+        return street
+    
+def checkFunc(line, map):
+    cmd = line[:3]  # First 3 letters of input contents
+    if(cmd == "add"):   # If the command is add
+        street = Street(line, map)  # Create a new street dictionary
+        Name = street.getStreetName()
+        Node = street.getNode()
+        map.append(street.capsulate(name=Name, node=Node))  # Add this dictionary in map list
+        print(map)
+
+    elif(cmd == "mod"):
+        street = Street(line, map)
+        Name = street.getStreetName()
+        Node = street.getNode()
+        streetQuantity = len(map)
+        for i in range(streetQuantity):
+            if(map[i]["Name"] == Name):
+                map.remove(map[i])  # First delete
+                map.append(street.capsulate(name=Name, node=Node))  # Then add
+        print(map)
+
+    elif(cmd == "rm "):
+        length = len(line)
+        index = []
+        for i in range(length):
+            if(line[i] == '"'): # If we found "
+                index.append(i) # return the index of two " in line
+        Name = line[index[0]+1: index[1]] # Contents in the middle of two "
+
+        streetNum = len(map)
+        for i in range(streetNum):
+            if(map[i]["Name"] == Name): # Find we want to delete according to name
+                map.remove(map[i])
+                break
+        print(map)
+    elif(cmd == "gg "):
+        ...
+    
     else:
         print("Error")
-
 
 def main():
     # YOUR MAIN CODE GOES HERE
@@ -62,11 +79,12 @@ def main():
     # sample code to read from stdin.
     # make sure to remove all spurious print statements as required
     # by the assignment
+    map = []
     while True:
         line = sys.stdin.readline()
-        if line == "":
+        if line == ' ':
             break
-        check_func(line)
+        checkFunc(line, map)
 
     print("Finished reading input")
     # return exit code 0 on successful termination
@@ -75,3 +93,6 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+# add "A Street" (1,2) (3,4)
+# [{'Name': 'A Street', 'Node': ['1', '2', '3', '4']}, {'Name': 'B Street', 'Node': ['1', '2', '3', '4']}]
