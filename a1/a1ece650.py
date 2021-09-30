e894f870652c3ecc3765ea1141b4e04eb6b8ddea
Kunyang Xie <k47xie@uwaterloo.ca>
2021-09-29 20:17:08 -0400

    Finish some small bugs

--- a1/a1ece650.py
+++ a1/a1ece650.py
@@ -12,18 +12,33 @@ class Street:
         length = len(self.command)
         index = []
         for i in range(length):
-            if(self.command[i] == '"'): # If we found "
-                index.append(i) # return the index of two " in line
-        streetName = self.command[index[0]+1: index[1]] # Contents in the middle of two "
+            if(self.command[i] == '"'): # If we found '"'
+                index.append(i) # return the index of two '"' in line
+        streetName = self.command[index[0]+1: index[1]] # Contents in the middle of two '"'
         return streetName
 
     def getNode(self):
-        length = len(self.command)
+        num = []    # Coordinate number
         node = []
-        for i in range(length):
-            if(self.command[i] == ','): # If we found ,
-                node.append(self.command[i - 1])  
-                node.append(self.command[i + 1])    # add content before and after ,
+        for i in range(len(self.command)):
+            if(self.command[i] == '('): # If we found '('
+                char = ''
+                j = i
+                while(self.command[j + 1] != ','):   # Until ','
+                    char = char + self.command[j + 1]
+                    j = j + 1
+                char = int(char)
+                num.append(char)
+            if(self.command[i] == ','): # If we found ','
+                char = ''
+                j = i
+                while(self.command[j + 1] != ')'):   # Until ')'
+                    char = char + self.command[j + 1]
+                    j = j + 1
+                char = int(char)
+                num.append(char)
+        for i in range(0, len(num), 2):
+            node.append((num[i], num[i + 1]))
         return node
     
     def capsulate(self, name, node):    # Capsulate data into dictionary
@@ -32,46 +47,101 @@ class Street:
             "Node": node
         }
         return street
-    
+        
+def findIntersection(x1, y1, x2, y2, x3, y3, x4, y4):
+    # (x1, y1) and (x2, y2) is in a line
+    # (x3, y3) and (x4, y4) is in a line
+    if(((x1-x2)*(y3-y4)-(y1-y2)*(x3-x4)) == 0): # Parallel
+        return 0
+    else:
+        px= ((x1*y2-y1*x2)*(x3-x4)-(x1-x2)*(x3*y4-y3*x4)) / ((x1-x2)*(y3-y4)-(y1-y2)*(x3-x4)) 
+        py= ((x1*y2-y1*x2)*(y3-y4)-(y1-y2)*(x3*y4-y3*x4)) / ((x1-x2)*(y3-y4)-(y1-y2)*(x3-x4))
+        return px, py   # Intersection
+
+def decodeNode(dict):
+    lengthNum = len(dict["Node"])
+    point = []
+    for i in range(0, lengthNum, 2):
+        point.append((int(dict["Node"][i]), int(dict["Node"][i + 1])))
+    return point
+
 def checkFunc(line, map):
     cmd = line[:3]  # First 3 letters of input contents
     if(cmd == "add"):   # If the command is add
         street = Street(line, map)  # Create a new street dictionary
         Name = street.getStreetName()
         Node = street.getNode()
-        map.append(street.capsulate(name=Name, node=Node))  # Add this dictionary in map list
+        alreadyIn = []  # Streets which have already added
+        if(len(map) == 0):
+            map.append(street.capsulate(name=Name, node=Node))  # Add this dictionary in map list
+        else:
+            for i in range(len(map)):
+                alreadyIn.append(map[i]["Name"])
+            if(Name in alreadyIn):
+                print('Warning: The street "{}" has already been added!'.format(Name))
+            else:
+                map.append(street.capsulate(name=Name, node=Node))  # Add this dictionary in map list
         print(map)
 
     elif(cmd == "mod"):
         street = Street(line, map)
         Name = street.getStreetName()
         Node = street.getNode()
-        streetQuantity = len(map)
-        for i in range(streetQuantity):
-            if(map[i]["Name"] == Name):
-                map.remove(map[i])  # First delete
-                map.append(street.capsulate(name=Name, node=Node))  # Then add
+        alreadyIn = []
+        for i in range(len(map)):
+            alreadyIn.append(map[i]["Name"])
+        if(Name in alreadyIn):
+            map.remove(map[i])  # First delete
+            map.append(street.capsulate(name=Name, node=Node))  # Then add
+        else:
+            print('Error: There is no such street called "{}"!'.format(Name))    
         print(map)
 
     elif(cmd == "rm "):
-        length = len(line)
         index = []
-        for i in range(length):
+        for i in range(len(line)):
             if(line[i] == '"'): # If we found "
                 index.append(i) # return the index of two " in line
         Name = line[index[0]+1: index[1]] # Contents in the middle of two "
-
-        streetNum = len(map)
-        for i in range(streetNum):
-            if(map[i]["Name"] == Name): # Find we want to delete according to name
-                map.remove(map[i])
-                break
+        alreadyIn = []
+        for i in range(len(map)):
+            alreadyIn.append(map[i]["Name"])
+        if(Name in alreadyIn):
+            map.remove(map[i])  # Delete
+        else:
+            print('Error: There is no such street called "{}"!'.format(Name)) 
         print(map)
-    elif(cmd == "gg "):
-        ...
-    
+
+    elif(cmd[:2] == "gg"):
+        intersection = {}
+        endpoint = {}
+        V = {}
+        E = {}
+        if(len(map) == 1):
+            print("V =", V)
+            print("E =", E)
+        else:
+            for i in range(len(map)):
+                chosenStreet = map[i]
+                otherStreet = map[:i] + map[i + 1:]
+                chosenPoint = decodeNode(chosenStreet)
+                print(chosenPoint)
+                # otherPoint = decodeNode(otherStreet)
+                # print(chosenPoint)
+                # print(otherPoint)
+
+
+    elif(cmd[0] == ' '):    # Finish code
+        print("Finished reading input")
+        # return exit code 0 on successful termination
+        sys.exit(0)
     else:
-        print("Error")
+        command = ""
+        for i in range(len(line)):
+            command = command + line[i]
+            if(line[i + 1] == ' '):
+                break
+        print('Error: There is no such command called "{}"!'.format(command))
 
 def main():
     # YOUR MAIN CODE GOES HERE
@@ -82,17 +152,12 @@ def main():
     map = []
     while True:
         line = sys.stdin.readline()
-        if line == ' ':
-            break
         checkFunc(line, map)
-
-    print("Finished reading input")
-    # return exit code 0 on successful termination
-    sys.exit(0)
-
 
 if __name__ == "__main__":
     main()
 
-# add "A Street" (1,2) (3,4)
-# [{'Name': 'A Street', 'Node': ['1', '2', '3', '4']}, {'Name': 'B Street', 'Node': ['1', '2', '3', '4']}]
+# add "A Street" (1,2) (3,4) (5,6)
+# add "B Street" (11,2) (3,14) (15,6)
+# add "C Street" (11,12) (13,14) (15,16) 
+# [{'Name': 'A Street', 'Node': [(1, 2), (3, 4), (5, 6)]}, {'Name': 'B Street', 'Node': [(11, 2), (3, 14), (15, 6)]}]
