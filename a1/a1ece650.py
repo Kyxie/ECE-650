bcf5f9d26650b1cbe9aec52d1255bffb6951c4dd
Kunyang Xie <k47xie@uwaterloo.ca>
2021-09-30 13:50:23 -0400

    Changed to object

--- a1/a1ece650.py
+++ a1/a1ece650.py
@@ -46,7 +46,146 @@ class Street:
             "Node": node
         }
         return street
-        
+    
+class Map:
+    def __init__(self, line, map):
+        self.line = line
+        self.map = map
+    
+    def checkFunc(self):
+        if(self.line[:3] == "add"):  # If the command is add
+            self.add()
+
+        elif(self.line[:3] == "mod"):
+            self.mod()
+
+        elif(self.line[:2] == "rm"):
+            self.rm()
+
+        elif(self.line[:2] == "gg"):
+            self.gg()
+
+        elif(self.line[0] == ' '):    # Finish code
+            print("Finished reading input")
+            # return exit code 0 on successful termination
+            sys.exit(0)
+
+        else:
+            command = ""
+            for i in range(len(self.line)):
+                command = command + self.line[i]
+                if(self.line[i + 1] == ' '):
+                    break
+            print('Error: There is no such command called "{}"!'.format(command))
+    
+    def add(self):
+        if(self.line[3] == ' '):
+            if(self.line[4] == '"' or self.line[4] == "'"):
+                leftBracketIndex = []
+                leftBracket = 0
+                rightBracket = 0
+                for i in range(len(self.line)):
+                    if(self.line[i] == '('):
+                        leftBracket = leftBracket + 1
+                        leftBracketIndex.append(i)
+                    if(self.line[i] == ')'):
+                        rightBracket = rightBracket + 1
+                if(leftBracket == 0 or rightBracket == 0 or leftBracket != rightBracket):
+                    print("Error: Vertices assignment error!")
+                elif(self.line[min(leftBracketIndex) - 1] != ' '):
+                    print("Error: Expect a space between street name and vertice!")
+                else:
+                    street = Street(self.line, self.map)  # Create a new street dictionary
+                    Name = street.getStreetName()
+                    Node = street.getNode()
+                    alreadyIn = []  # Streets which have already added
+                    if(len(self.map) == 0):
+                        self.map.append(street.capsulate(name=Name, node=Node))  # Add this dictionary in map list
+                    else:
+                        for i in range(len(self.map)):
+                            alreadyIn.append(self.map[i]["Name"])
+                        if(Name in alreadyIn):
+                            print('Warning: The street "{}" has already been added!'.format(Name))
+                        else:
+                            self.map.append(street.capsulate(name=Name, node=Node))  # Add this dictionary in map list
+            else:
+                print("Error: Except a \" or \' around street name!")
+        else:
+            print("Error: Expect a space between the command and street name!")
+        print(self.map)
+    
+    def mod(self):
+        if(self.line[3] == ' '):
+            if(self.line[4] == '"' or self.line[4] == "'"):
+                leftBracketIndex = []
+                leftBracket = 0
+                rightBracket = 0
+                for i in range(len(self.line)):
+                    if(self.line[i] == '('):
+                        leftBracket = leftBracket + 1
+                        leftBracketIndex.append(i)
+                    if(self.line[i] == ')'):
+                        rightBracket = rightBracket + 1
+                if(leftBracket == 0 or rightBracket == 0 or leftBracket != rightBracket):
+                    print("Error: Vertices assignment error!")
+                elif(self.line[min(leftBracketIndex) - 1] != ' '):
+                    print("Error: Expect a space between street name and vertice!")
+                else:
+                    street = Street(self.line, self.map)
+                    Name = street.getStreetName()
+                    Node = street.getNode()
+                    alreadyIn = []
+                    for i in range(len(self.map)):
+                        alreadyIn.append(self.map[i]["Name"])
+                    if(Name in alreadyIn):
+                        self.map.remove(self.map[i])  # First delete
+                        self.map.append(street.capsulate(name=Name, node=Node))  # Then add
+                    else:
+                        print('Error: There is no such street called "{}"!'.format(Name))
+            else:
+                print("Error: Except a \" or \' around street name!")
+        else:
+            print("Error: Expect a space between the command and street name!")
+        # # print(self.map)
+    
+    def rm(self):
+        if(self.line[2] == ' '):
+            if(self.line[3] == '"' or self.line[3] == "'"):
+                index = []
+                for i in range(len(self.line)):
+                    if(self.line[i] == '"'): # If we found "
+                        index.append(i) # return the index of two " in line
+                Name = self.line[index[0]+1: index[1]] # Contents in the middle of two "
+                alreadyIn = []
+                for i in range(len(self.map)):
+                    alreadyIn.append(self.map[i]["Name"])
+                if(Name in alreadyIn):
+                    map.remove(self.map[i])  # Delete
+                else:
+                    print('Error: There is no such street called "{}"!'.format(Name))
+            else:
+                print("Error: Expect a \" or \' around street name!")
+        else:
+            print("Error: Expect a space between command and street name!")
+        # print(self.map)
+    
+    def gg(self):
+        intersection = {}
+        endpoint = {}
+        V = {}
+        E = {}
+        if(len(self.map) == 0 or len(self.map) == 1):
+            print("V =", V)
+            print("E =", E)
+        else:
+            for i in range(len(self.map)):
+                chosenStreet = [self.map[i]] # Change to a list
+                otherStreet = self.map[:i] + self.map[i + 1:]
+                chosenPoint = decodeNode(chosenStreet)  # List of points
+                otherPoint = decodeNode(otherStreet)
+                # for j in range():
+                #     ...
+
 def findIntersection(x1, y1, x2, y2, x3, y3, x4, y4):
     # (x1, y1) and (x2, y2) is in a line
     # (x3, y3) and (x4, y4) is in a line
@@ -92,129 +231,6 @@ def decodeNode(list):
         point.append(set(list[i]["Node"]))
     return point
 
-def checkFunc(line, map):
-    if(line[:3] == "add"):  # If the command is add
-        if(line[3] == ' '):
-            if(line[4] == '"' or line[4] == "'"):
-                leftBracketIndex = []
-                leftBracket = 0
-                rightBracket = 0
-                for i in range(len(line)):
-                    if(line[i] == '('):
-                        leftBracket = leftBracket + 1
-                        leftBracketIndex.append(i)
-                    if(line[i] == ')'):
-                        rightBracket = rightBracket + 1
-                if(leftBracket == 0 or rightBracket == 0 or leftBracket != rightBracket):
-                    print("Error: Vertices assignment error!")
-                elif(line[min(leftBracketIndex) - 1] != ' '):
-                    print("Error: Expect a space between street name and vertice!")
-                else:
-                    street = Street(line, map)  # Create a new street dictionary
-                    Name = street.getStreetName()
-                    Node = street.getNode()
-                    alreadyIn = []  # Streets which have already added
-                    if(len(map) == 0):
-                        map.append(street.capsulate(name=Name, node=Node))  # Add this dictionary in map list
-                    else:
-                        for i in range(len(map)):
-                            alreadyIn.append(map[i]["Name"])
-                        if(Name in alreadyIn):
-                            print('Warning: The street "{}" has already been added!'.format(Name))
-                        else:
-                            map.append(street.capsulate(name=Name, node=Node))  # Add this dictionary in map list
-            else:
-                print("Error: Except a \" or \' around street name!")
-        else:
-            print("Error: Expect a space between the command and street name!")
-        # print(map)
-
-    elif(line[:3] == "mod"):
-        if(line[3] == ' '):
-            if(line[4] == '"' or line[4] == "'"):
-                leftBracketIndex = []
-                leftBracket = 0
-                rightBracket = 0
-                for i in range(len(line)):
-                    if(line[i] == '('):
-                        leftBracket = leftBracket + 1
-                        leftBracketIndex.append(i)
-                    if(line[i] == ')'):
-                        rightBracket = rightBracket + 1
-                if(leftBracket == 0 or rightBracket == 0 or leftBracket != rightBracket):
-                    print("Error: Vertices assignment error!")
-                elif(line[min(leftBracketIndex) - 1] != ' '):
-                    print("Error: Expect a space between street name and vertice!")
-                else:
-                    street = Street(line, map)
-                    Name = street.getStreetName()
-                    Node = street.getNode()
-                    alreadyIn = []
-                    for i in range(len(map)):
-                        alreadyIn.append(map[i]["Name"])
-                    if(Name in alreadyIn):
-                        map.remove(map[i])  # First delete
-                        map.append(street.capsulate(name=Name, node=Node))  # Then add
-                    else:
-                        print('Error: There is no such street called "{}"!'.format(Name))
-            else:
-                print("Error: Except a \" or \' around street name!")
-        else:
-            print("Error: Expect a space between the command and street name!")
-        # # print(map)
-
-    elif(line[:2] == "rm"):
-        if(line[2] == ' '):
-            if(line[3] == '"' or line[3] == "'"):
-                index = []
-                for i in range(len(line)):
-                    if(line[i] == '"'): # If we found "
-                        index.append(i) # return the index of two " in line
-                Name = line[index[0]+1: index[1]] # Contents in the middle of two "
-                alreadyIn = []
-                for i in range(len(map)):
-                    alreadyIn.append(map[i]["Name"])
-                if(Name in alreadyIn):
-                    map.remove(map[i])  # Delete
-                else:
-                    print('Error: There is no such street called "{}"!'.format(Name))
-            else:
-                print("Error: Expect a \" or \' around street name!")
-        else:
-            print("Error: Expect a space between command and street name!")
-        # print(map)
-
-    elif(line[:2] == "gg"):
-        intersection = {}
-        endpoint = {}
-        V = {}
-        E = {}
-        if(len(map) == 0 or len(map) == 1):
-            print("V =", V)
-            print("E =", E)
-        else:
-            for i in range(len(map)):
-                chosenStreet = [map[i]] # Change to a list
-                otherStreet = map[:i] + map[i + 1:]
-                chosenPoint = decodeNode(chosenStreet)  # List of points
-                otherPoint = decodeNode(otherStreet)
-                # for j in range():
-                #     ...
-
-
-    elif(line[0] == ' '):    # Finish code
-        print("Finished reading input")
-        # return exit code 0 on successful termination
-        sys.exit(0)
-
-    else:
-        command = ""
-        for i in range(len(line)):
-            command = command + line[i]
-            if(line[i + 1] == ' '):
-                break
-        print('Error: There is no such command called "{}"!'.format(command))
-
 def main():
     # YOUR MAIN CODE GOES HERE
 
@@ -224,7 +240,8 @@ def main():
     map = []
     while True:
         line = sys.stdin.readline()
-        checkFunc(line, map)
+        world = Map(line, map)
+        world.checkFunc()
 
 if __name__ == "__main__":
     main()
