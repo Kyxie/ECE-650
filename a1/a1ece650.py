e1db8bb8baf849075ad5d8ddfcd863f917d260b6
Kunyang Xie <k47xie@uwaterloo.ca>
2021-09-30 21:44:18 -0400

    Small E bug

--- a1/a1ece650.py
+++ a1/a1ece650.py
@@ -72,10 +72,8 @@ class Map:
 
         else:
             command = ""
-            for i in range(len(self.line)):
+            for i in range(len(self.line) - 1):
                 command = command + self.line[i]
-                if(self.line[i + 1] == ' '):
-                    break
             print('Error: There is no such command called "{}"!'.format(command))
     
     def add(self):
@@ -112,7 +110,7 @@ class Map:
                 print("Error: Except a \" or \' around street name!")
         else:
             print("Error: Expect a space between the command and street name!")
-        print(self.map)
+        # print(self.map)
     
     def mod(self):
         if(self.line[3] == ' '):
@@ -146,7 +144,7 @@ class Map:
                 print("Error: Except a \" or \' around street name!")
         else:
             print("Error: Expect a space between the command and street name!")
-        # # print(self.map)
+        # print(self.map)
     
     def rm(self):
         if(self.line[2] == ' '):
@@ -170,21 +168,68 @@ class Map:
         # print(self.map)
     
     def gg(self):
-        intersection = {}
-        endpoint = {}
+        intersection = []
+        endPoint = []
         V = {}
-        E = {}
+        E = []
         if(len(self.map) == 0 or len(self.map) == 1):
-            print("V =", V)
-            print("E =", E)
+            print("V = {\n}")
+            print("E = {\n}")
         else:
             for i in range(len(self.map)):
                 chosenStreet = [self.map[i]] # Change to a list
-                otherStreet = self.map[:i] + self.map[i + 1:]
+                otherStreet = self.map[i + 1:]
                 chosenPoint = decodeNode(chosenStreet)  # List of points
                 otherPoint = decodeNode(otherStreet)
-                # for j in range():
-                #     ...
+                for j in range(len(otherPoint)):
+                    for k in range(len(chosenPoint[0]) - 1):
+                        for l in range(len(otherPoint[j]) - 1):
+                            x1 = chosenPoint[0][k][0]
+                            y1 = chosenPoint[0][k][1]
+                            x2 = chosenPoint[0][k + 1][0]
+                            y2 = chosenPoint[0][k + 1][1]
+                            x3 = otherPoint[j][l][0]
+                            y3 = otherPoint[j][l][1]
+                            x4 = otherPoint[j][l + 1][0]
+                            y4 = otherPoint[j][l + 1][1]
+                            (px, py) = findIntersection(x1, y1, x2, y2, x3, y3, x4, y4)
+                            if((px, py) != ('F', 'F')):
+                                intersection.append((px, py))   # Intersection
+                                endPoint.append((x1, y1))
+                                endPoint.append((x2, y2))
+                                endPoint.append((x3, y3))
+                                endPoint.append((x4, y4))   # The 4 points which form an intersection
+            vertices = list(set(intersection + endPoint))    # Delete the duplication and turn to a list
+            for i, vertice in enumerate(vertices):
+                V[i] = vertice
+            for i in range(0, len(endPoint), 4):
+                leftPoint = endPoint[i]
+                for j in range(len(V)):
+                    if(V[j] == leftPoint):
+                        E.append(j + 1)
+                rightPoint = endPoint[i + 1]
+                for j in range(len(V)):
+                    if(V[j] == rightPoint):
+                        E.append(j + 1)
+                upPoint = endPoint[i + 2]
+                for j in range(len(V)):
+                    if(V[j] == upPoint):
+                        E.append(j + 1)
+                downPoint = endPoint[i + 3]
+                for j in range(len(V)):
+                    if(V[j] == downPoint):
+                        E.append(j + 1)
+        printResult(V, E)
+
+def printResult(V, E):
+    print("V = {")
+    for i in range(len(V)):
+        print("{}:  ({:.2f},{:.2f})".format(i + 1, V[i][0], V[i][1]))
+    print("}")
+    print("E = {")
+    for i in range(0, len(E), 2):
+        print("<{},{}>,".format(E[i], E[i + 1]))
+    print("}")
 
 def findIntersection(x1, y1, x2, y2, x3, y3, x4, y4):
     # (x1, y1) and (x2, y2) is in a line
@@ -228,7 +273,7 @@ def findIntersection(x1, y1, x2, y2, x3, y3, x4, y4):
 def decodeNode(list):
     point = []
     for i in range(len(list)):
-        point.append(set(list[i]["Node"]))
+        point.append((list[i]["Node"]))
     return point
 
 def main():
@@ -246,7 +291,6 @@ def main():
 if __name__ == "__main__":
     main()
 
-# add "A Street" (1,2) (3,4) (5,6)
-# add "B Street" (11,2) (3,14) (15,6)
-# add "C Street" (11,12) (13,14) (15,16) 
-# [{'Name': 'A Street', 'Node': [(1, 2), (3, 4), (5, 6)]}, {'Name': 'B Street', 'Node': [(11, 2), (3, 14), (15, 6)]}]
+# add "Weber Street" (2,-1)(2,2)(5,5)(5,6)(3,8)
+# add "King Street" (4,2)(4,8)
+# add "Davenport Road" (1,4)(5,8)
