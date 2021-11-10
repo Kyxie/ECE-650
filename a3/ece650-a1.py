186087f02f7b3102993156c5cb619590618a5044
Kunyang Xie <k47xie@uwaterloo.ca>
2021-11-09 19:08:18 -0500

    A3 Init

--- a3/ece650-a1.py
+++ a3/ece650-a1.py
@@ -1,4 +1,422 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+import sys
+import re
 
-if __name__ == '__main__':
-    print 'Replace with your solution for Assignment 1'
+# YOUR CODE GOES HERE
+
+
+class Street:
+    def __init__(self, command, map):
+        self.command = command
+        self.map = map
+
+    def getStreetName(self):
+        index = []
+        for i in range(len(self.command)):
+            if(self.command[i] == '"' or self.command[i] == "'"):  # If we found '"' or "'"
+                index.append(i)  # return the index of two '"' in line
+        # Contents in the middle of two '"'
+        streetName = self.command[index[0] + 1: index[1]]
+        return streetName
+
+    def getNode(self):
+        num = []    # Coordinate number
+        node = []
+        for i in range(len(self.command)):
+            if(self.command[i] == '('):  # If we found '('
+                char = ''
+                j = i
+                while(self.command[j + 1] != ','):   # Until ','
+                    char = char + self.command[j + 1]
+                    j = j + 1
+                char = int(char)
+                num.append(char)
+            if(self.command[i] == ','):  # If we found ','
+                char = ''
+                j = i
+                while(self.command[j + 1] != ')'):   # Until ')'
+                    char = char + self.command[j + 1]
+                    j = j + 1
+                char = int(char)
+                num.append(char)
+        for i in range(0, len(num), 2):
+            node.append((num[i], num[i + 1]))
+        return node
+
+    def capsulate(self, name, node):    # Capsulate data into dictionary
+        street = {
+            "Name": name,
+            "Node": node
+        }
+        return street
+
+
+class Map:
+    def __init__(self, line, map):
+        self.line = line
+        self.map = map
+
+    def checkFunc(self):
+        cmd = re.search(r'\s', self.line)
+        if(self.line[:3] == "add"):  # If the command is add
+            self.add()
+
+        elif(self.line[:3] == "mod"):
+            self.mod()
+
+        elif(self.line[:2] == "rm"):
+            self.rm()
+
+        elif(self.line[:2] == "gg"):
+            self.gg()
+
+        elif(self.line[:3] == "map"):
+            print(self.map)
+
+        elif(self.line == ""):
+            ...
+
+        else:
+            command = ""
+            for i in range(len(self.line) - 1):
+                command = command + self.line[i]
+            print('Error: There is no such command called "{}"!'.format(command))
+
+    def add(self):
+        if(self.line[3] == ' '):
+            if(self.line[4] == '"' or self.line[4] == "'"):
+                leftBracketIndex = []
+                leftBracket = 0
+                rightBracket = 0
+                for i in range(len(self.line)):
+                    if(self.line[i] == '('):
+                        leftBracket = leftBracket + 1
+                        leftBracketIndex.append(i)
+                    if(self.line[i] == ')'):
+                        rightBracket = rightBracket + 1
+                if(leftBracket == 0 or rightBracket == 0 or leftBracket != rightBracket):
+                    print("Error: Vertices assignment error!")
+                elif(self.line[min(leftBracketIndex) - 1] != ' '):
+                    print("Error: Expect a space between street name and vertice!")
+                else:
+                    do = 1
+                    # Create a new street dictionary
+                    street = Street(self.line, self.map)
+                    Name = street.getStreetName().lower()
+
+                    if(Name[0] == ' ' or Name[-1] == ' '):
+                        print("Error: No Leading or following white space!")
+                        do = 0
+
+                    moreSpace = re.search(r'\s{2,10}', Name)
+                    if(moreSpace):
+                        print("Error: No more than 1 white space in street name!")
+                        do = 0
+
+                    plus = re.search(r'\+', self.line)
+                    if(plus):
+                        print("Error: Input positive number without '+' sign!")
+                        do = 0
+
+                    numOfBracket = 0
+                    for i in range(len(self.line)):
+                        if(self.line[i] == '('):
+                            numOfBracket = numOfBracket + 1
+                    if(numOfBracket == 1 or numOfBracket == 0):
+                        print("Error: Not enough points for a line")
+                        do = 0
+
+                    specialCharacter = "~!@#$%^&*()_+`1234567890-=\{\}|[]\\;:,./<>?"
+                    for i in range(len(specialCharacter)):
+                        if(specialCharacter[i] in Name):
+                            print("Error: No special characters or numbers!")
+                            do = 0
+                            break
+
+                    Node = street.getNode()
+                    alreadyIn = []  # Streets which have already added
+                    if(do == 1):
+                        if(len(self.map) == 0):
+                            # Add this dictionary in map list
+                            self.map.append(
+                                street.capsulate(name=Name, node=Node))
+                        else:
+                            for i in range(len(self.map)):
+                                alreadyIn.append(self.map[i]["Name"])
+                            if(Name in alreadyIn):
+                                print(
+                                    'Error: The street "{}" has already been added!'.format(Name))
+                            else:
+                                # Add this dictionary in map list
+                                self.map.append(
+                                    street.capsulate(name=Name, node=Node))
+            else:
+                print("Error: Except a \" or \' around street name!")
+        else:
+            print("Error: Expect a space between the command and street name!")
+        # print(self.map)
+
+    def mod(self):
+        if(self.line[3] == ' '):
+            if(self.line[4] == '"' or self.line[4] == "'"):
+                leftBracketIndex = []
+                leftBracket = 0
+                rightBracket = 0
+                for i in range(len(self.line)):
+                    if(self.line[i] == '('):
+                        leftBracket = leftBracket + 1
+                        leftBracketIndex.append(i)
+                    if(self.line[i] == ')'):
+                        rightBracket = rightBracket + 1
+                if(leftBracket == 0 or rightBracket == 0 or leftBracket != rightBracket):
+                    print("Error: Vertices assignment error!")
+                elif(self.line[min(leftBracketIndex) - 1] != ' '):
+                    print("Error: Expect a space between street name and vertice!")
+                else:
+                    do = 1
+
+                    numOfBracket = 0
+                    for i in range(len(self.line)):
+                        if(self.line[i] == '('):
+                            numOfBracket = numOfBracket + 1
+                    if(numOfBracket == 1 or numOfBracket == 0):
+                        print("Error: Not enough points for a line")
+                        do = 0
+
+                    if(do == 1):
+                        street = Street(self.line, self.map)
+                        Name = street.getStreetName().lower()
+                        Node = street.getNode()
+                        alreadyIn = []
+                        for i in range(len(self.map)):
+                            alreadyIn.append(self.map[i]["Name"])
+                            if(alreadyIn[i] == Name):
+                                self.map[i] = street.capsulate(
+                                    name=Name, node=Node)
+                                break
+                        if(Name not in alreadyIn):
+                            print(
+                                'Error: There is no such street called "{}"!'.format(Name))
+            else:
+                print("Error: Except a \" or \' around street name!")
+        else:
+            print("Error: Expect a space between the command and street name!")
+        # print(self.map)
+
+    def rm(self):
+        if(self.line[2] == ' '):
+            if(self.line[3] == '"' or self.line[3] == "'"):
+                do = 1
+
+                if(do == 1):
+                    index = []
+                    for i in range(len(self.line)):
+                        if(self.line[i] == '"'):  # If we found "
+                            # return the index of two " in line
+                            index.append(i)
+                    # Contents in the middle of two "
+                    Name = self.line[index[0]+1: index[1]]
+                    Name = Name.lower()
+                    alreadyIn = []
+                    for i in range(len(self.map)):
+                        alreadyIn.append(self.map[i]["Name"])
+                        if(Name in alreadyIn):
+                            self.map.remove(self.map[i])  # Delete
+                            break
+                    if(Name not in alreadyIn):
+                        print(
+                            'Error: There is no such street called "{}"!'.format(Name))
+            else:
+                print("Error: Expect a \" or \' around street name!")
+        else:
+            print("Error: Expect a space between command and street name!")
+        # print(self.map)
+
+    def gg(self):
+        do = 1
+        if(do == 1):
+            V = {}
+            E = []
+            intersection = []
+            endPoint = []
+            package = []
+            if(len(self.map) == 0 or len(self.map) == 1):
+                print("V = {\n}")
+                print("E = {\n}")
+            else:
+                for i in range(len(self.map)):
+                    chosenStreet = [self.map[i]]  # Change to a list
+                    otherStreet = self.map[i + 1:]
+                    chosenPoint = decodeNode(chosenStreet)  # List of points
+                    otherPoint = decodeNode(otherStreet)
+                    for j in range(len(otherPoint)):
+                        for k in range(len(chosenPoint[0]) - 1):
+                            for l in range(len(otherPoint[j]) - 1):
+                                x1 = chosenPoint[0][k][0]
+                                y1 = chosenPoint[0][k][1]
+                                x2 = chosenPoint[0][k + 1][0]
+                                y2 = chosenPoint[0][k + 1][1]
+                                x3 = otherPoint[j][l][0]
+                                y3 = otherPoint[j][l][1]
+                                x4 = otherPoint[j][l + 1][0]
+                                y4 = otherPoint[j][l + 1][1]
+                                (px, py) = findIntersection(
+                                    x1, y1, x2, y2, x3, y3, x4, y4)
+                                if((px, py) != ('F', 'F')):
+                                    intersection.append(
+                                        (px, py))   # Intersection
+                                    endPoint.append((x1, y1))
+                                    endPoint.append((x2, y2))
+                                    endPoint.append((x3, y3))
+                                    # The 4 edges which form an intersection
+                                    endPoint.append((x4, y4))
+                                    package.append(
+                                        ((px, py), (x1, y1), (x2, y2), (x3, y3), (x4, y4)))
+                # Delete the duplication and turn to a list
+                vertices = list(set(intersection + endPoint))
+                for i, vertice in enumerate(vertices):
+                    V[i] = vertice
+                for i in range(len(intersection)):
+                    pi = intersection[i]    # The intersection
+                    otherInter = intersection[:i] + intersection[i + 1:]
+                    if(len(otherInter) == 0):
+                        for j in range(1, 5):
+                            pj = package[i][j]
+                            isInE(E, pi, pj)
+                    else:
+                        for j in range(1, 5):  # An intersection is caused by 4 endpoints
+                            pj = package[i][j]  # The end point
+                            for k in range(len(otherInter)):
+                                q = otherInter[k]   # The other intersection
+                                if(isPointOnSeg(q, pi, pj) == 1):
+                                    isInE(E, pi, q)
+                                elif(isPointOnSeg(q, pi, pj) == 2):
+                                    isInE(E, pi, pj)
+                                else:
+                                    continue
+                for i in range(len(E)):
+                    for j in range(2):
+                        for k in range(len(V)):
+                            if(E[i][j] == V[k]):
+                                E[i][j] = k + 1
+                printResult(V, E)
+
+
+def printResult(V, E):
+    print("V = {")
+    for i in range(len(V)):
+        print("   {}:  ({:.2f},{:.2f})".format(i + 1, V[i][0], V[i][1]))
+    print("}")
+    print("E = {")
+    for i in range(len(E) - 1):
+        print("   <{},{}>,".format(E[i][0], E[i][1]))
+    print("   <{},{}>".format(E[len(E) - 1][0], E[len(E) - 1][1]))
+    print("}")
+
+
+def findIntersection(x1, y1, x2, y2, x3, y3, x4, y4):
+    # (x1, y1) and (x2, y2) is in a line
+    # (x3, y3) and (x4, y4) is in a line
+    if(((x1-x2)*(y3-y4)-(y1-y2)*(x3-x4)) != 0):  # Not Parallel
+        px = ((x1*y2-y1*x2)*(x3-x4)-(x1-x2)*(x3*y4-y3*x4)) / \
+            ((x1-x2)*(y3-y4)-(y1-y2)*(x3-x4))
+        py = ((x1*y2-y1*x2)*(y3-y4)-(y1-y2)*(x3*y4-y3*x4)) / \
+            ((x1-x2)*(y3-y4)-(y1-y2)*(x3-x4))
+        if(x1 >= x2 and y1 >= y2):
+            if(px >= x2 and px <= x1 and py >= y2 and py <= y1):
+                return px, py
+            else:
+                px = 'F'
+                py = 'F'
+                return px, py
+        elif(x1 <= x2 and y1 >= y2):
+            if(px >= x1 and px <= x2 and py >= y2 and py <= y1):
+                return px, py
+            else:
+                px = 'F'
+                py = 'F'
+                return px, py
+        elif(x1 >= x2 and y1 <= y2):
+            if(px >= x2 and px <= x1 and py >= y1 and py <= y2):
+                return px, py
+            else:
+                px = 'F'
+                py = 'F'
+                return px, py
+        elif(x1 <= x2 and y1 <= y2):
+            if(px >= x1 and px <= x2 and py >= y1 and py <= y2):
+                return px, py
+            else:
+                px = 'F'
+                py = 'F'
+                return px, py
+    else:
+        px = 'F'
+        py = 'F'
+        return px, py
+
+
+def isPointOnSeg(q, pi, pj):
+    # Point[0] means x
+    # Point[1] means y
+    qx = q[0]
+    qy = q[1]
+    pix = pi[0]
+    piy = pi[1]
+    pjx = pj[0]
+    pjy = pj[1]
+    if((((qx - pix) * (pjy - piy) - (pjx - pix) * (qy - piy) < 0.01)
+            and ((qx - pix) * (pjy - piy) - (pjx - pix) * (qy - piy) > -0.01))):    # In a line
+        if(min(pix, pjx) <= qx and qx <= max(pix, pjx)
+           and min(piy, pjy) <= qy and qy <= max(piy, pjy)):    # In a line and between 2 points
+            return 1
+        else:   # In a line but not between 2 points
+            return 2
+    else:   # Not in a line
+        return 3
+
+
+def isInE(E, pi, pj):
+    # E: [[(x1, y1), (x2, y2)], [(x3, y3), (x4, y4)], ...]
+    # pi: (x1, y1)
+    # pj: (x2, y2)
+    if([pi, pj] not in E and [pj, pi] not in E and pi != pj):
+        E.append([pi, pj])
+
+
+def decodeNode(list):
+    point = []
+    for i in range(len(list)):
+        point.append((list[i]["Node"]))
+    return point
+
+
+def main():
+    # YOUR MAIN CODE GOES HERE
+
+    # sample code to read from stdin.
+    # make sure to remove all spurious print statements as required
+    # by the assignment
+    map = []
+    while True:
+        line = sys.stdin.readline().strip('')
+        world = Map(line, map)
+        world.checkFunc()
+        if line == "":
+            break
+    sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
+
+# Test
+# add "Weber Street" (2,-1) (2,2) (5,5) (5,6) (3,8)
+# add "King Street S" (3,2) (4,8)
+# add "Davenport Road" (0,0) (5,8)
+# mod "Weber Street" (2,1)(2,2)
+# rm "King Street"
+
+# add "Weber Street" (-1,0)(4,0)
+# add "King Street" (0,-1)(0,4)
+# add "Davenport Road" (0,3)(3,0)
--- a3/ece650-a2.cpp
+++ a3/ece650-a2.cpp
@@ -1,6 +1,231 @@
 #include <iostream>
+#include <vector>
 
-int main(void) {
-    std::cout << "Replace with your solution for Assignment 2\n";
+#define INF 100000
+
+using namespace std;
+
+int VNum;
+vector<int> first;
+vector<int> second;
+
+int V(string line)
+{
+    // vertex: int type
+    // vertice: string type
+    int vertex;
+    string vertice;
+    for (int i = 2; i <= line.length(); i++)
+        vertice[i - 2] = line[i];
+    vertex = atoi(vertice.c_str());
+    return vertex;
+}
+
+bool checkE(vector<int> side)
+{
+    bool EOK = true;
+    for (int i = 0; i < side.size(); i++)
+    {
+        if (side[i] > VNum)
+        {
+            EOK = false;
+            break;
+        }
+    }
+    return EOK;
+}
+
+bool alreadyIn(vector<int> alreadyInputs, int key)
+{
+    bool alreadyIn = false;
+    for (int i = 0; i < alreadyInputs.size(); i++)
+    {
+        if (key == alreadyInputs[i])
+        {
+            alreadyIn = true;
+            break;
+        }
+    }
+    return alreadyIn;
+}
+
+vector<int> findNumber(vector<int> first, vector<int> second)
+{
+    bool boolAlreadyIn = false;
+    vector<int> set;
+    for (int i = 0; i < first.size(); i++)
+    {
+        boolAlreadyIn = alreadyIn(set, first[i]);
+        if (boolAlreadyIn)
+            continue;
+        else
+            set.push_back(first[i]);
+    }
+    for (int i = 0; i < first.size(); i++)
+    {
+        boolAlreadyIn = alreadyIn(set, second[i]);
+        if (boolAlreadyIn)
+            continue;
+        else
+            set.push_back(second[i]);
+    }
+    return set;
+}
+
+int checks(int start, int end)
+{
+    // sOK = 1 means good
+    // sOK = 2 means s out of V
+    // sOK = 3 means no route
+    int sOK = 1;
+    if (start > VNum || end > VNum)
+        sOK = 2;
+    vector<int> set = findNumber(first, second);
+    bool noStart = alreadyIn(set, start);
+    bool noEnd = alreadyIn(set, end);
+    if (start <= VNum && end <= VNum && (noStart == false || noEnd == false))
+        sOK = 3;
+    return sOK;
+}
+
+vector<int> find(int start, int end)
+{
+    int distance[50];
+    int previous[50];
+    for (int i = 1; i <= VNum; i++)
+    {
+        previous[i] = INF;
+        if (i == start)
+            distance[i] = 0;
+        else
+            distance[i] = INF;
+    }
+    for (int i = 0; i < VNum - 1; i++)
+    {
+        for (int j = 0; j < first.size(); j++)
+        {
+            int from = first[j];
+            int to = second[j];
+            if (distance[from] > distance[to] + 1)
+            {
+                distance[from] = distance[to] + 1;
+                previous[from] = to;
+            }
+            if (distance[to] > distance[from] + 1)
+            {
+                distance[to] = distance[from] + 1;
+                previous[to] = from;
+            }
+        }
+    }
+
+    // for(int i = 1; i <= VNum; i++)
+    //     cout << "dist[" << i << "] = " << distance[i] << endl;
+    // cout << "**************************" << endl;
+    // for(int i = 1; i <= VNum; i++)
+    //     cout << "prev[" << i << "] = " << previous[i] << endl;
+
+    vector<int> route;
+    route.push_back(end);
+    while (route.back() != start)
+        if (previous[route.back()] <= VNum)
+            route.push_back(previous[route.back()]);
+        else
+            break;
+    return route;
+}
+
+void print(vector<int> route)
+{
+    for (int i = route.size() - 1; i >= 0; i--)
+    {
+        if (i != 0)
+            cout << route[i] << '-';
+        else
+            cout << route[i];
+    }
+    cout << endl;
+}
+
+int main()
+{
+    while (!cin.eof())
+    {
+        string line;
+        getline(cin, line);
+        if (line[0] == 'V')
+        {
+            first.clear();
+            second.clear();
+            VNum = V(line);
+        }
+        else if (line[0] == 'E')
+        {
+            for (int i = 3; i <= line.length(); i++)
+            {
+                if (line[i] == '<')
+                {
+                    char number[5] = {0};
+                    for (int j = 0; line[i + j + 1] != ','; j++)
+                        number[j] = line[i + j + 1];
+                    int number_int = atoi(number);
+                    first.push_back(number_int);
+                    continue;
+                }
+                if (line[i] == '>')
+                {
+                    int j;
+                    char number[5] = {0};
+                    char number_inv[5] = {0};
+                    for (j = 0; line[i - j - 1] != ','; j++)
+                        number[j] = line[i - j - 1];
+                    for (int k = 0; k <= j; k++)
+                        number_inv[k] = number[j - k - 1];
+                    int number_int = atoi(number_inv);
+                    second.push_back(number_int);
+                    continue;
+                }
+            }
+            if (checkE(first) == false || checkE(second) == false)
+            {
+                first.clear();
+                second.clear();
+                cout << "Error: Point in E is out of the range of point in V!" << endl;
+                continue;
+            }
+        }
+        else if (line[0] == 's')
+        {
+            int start;
+            int end;
+            char number[5] = {0};
+            int i;
+            for (i = 0; line[1 + i + 1] != ' '; i++)
+                number[i] = line[1 + i + 1];
+            start = atoi(number);
+            for (int j = 0; i + j + 2 <= line.length(); j++)
+                number[j] = line[i + j + 2];
+            end = atoi(number);
+            if (checks(start, end) == 1)
+            {
+                vector<int> route = find(start, end);
+                if (route.size() > 1)
+                    print(route);
+                else
+                    cout << "Error: No path exists!" << endl;
+                continue;
+            }
+            else if (checks(start, end) == 2)
+            {
+                cout << "Error: Point in s must be in the range of E and V!" << endl;
+                continue;
+            }
+            else
+            {
+                cout << "Error: No path exists!" << endl;
+                continue;
+            }
+        }
+    }
     return 0;
 }
