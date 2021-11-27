882ae1b01e737cbf9c052232b474a3ec53c870a5
k47xie <k47xie@uwaterloo.ca>
2021-11-27 12:57:15 -0500

    a4 init

--- a4/ece650-a4.cpp
+++ a4/ece650-a4.cpp
@@ -1,3 +1,15 @@
-int main(int argc, char **argv) {
- return 0;
+/*
+ * @Date: 2021-11-26 15:39:46
+ * @LastEditors: Kunyang Xie
+ * @LastEditTime: 2021-11-26 17:23:57
+ * @FilePath: /k47xie/a4/ece650-a4.cpp
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    return 0;
 }
